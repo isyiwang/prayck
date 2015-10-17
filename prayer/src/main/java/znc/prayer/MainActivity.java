4c7b67c13b0473e40e8b52b2c5534c77e74d1789
isyiwang <isaacwang@gmail.com>
2015-10-16 22:30:23 -0700

    Update background image appropriately

--- prayer/src/main/java/znc/prayer/MainActivity.java
+++ prayer/src/main/java/znc/prayer/MainActivity.java
@@ -3,11 +3,19 @@ package znc.prayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.squareup.picasso.Picasso;
+
 public class MainActivity extends AppCompatActivity {
+
+    private KenBurnsView mBackgroundView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBackgroundView = (KenBurnsView) findViewById(R.id.background);
+        Picasso.with(this).load(R.drawable.forest).fit().centerCrop().into(mBackgroundView);
     }
 }
