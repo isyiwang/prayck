96bb2677e7d2f3c29d436db11c76e49fbad18350
isyiwang <isaacwang@gmail.com>
2015-10-16 23:41:08 -0700

    Image works

--- prayer/src/main/java/znc/prayer/MainActivity.java
+++ prayer/src/main/java/znc/prayer/MainActivity.java
@@ -1,21 +1,65 @@
 package znc.prayer;
 
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.ImageView;
 
 import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 public class MainActivity extends AppCompatActivity {
 
-    private KenBurnsView mBackgroundView;
+    private ImageView mBackgroundView;
+    private KenBurnsView mBlurredBackgroundView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBackgroundView = (KenBurnsView) findViewById(R.id.background);
-        Picasso.with(this).load(R.drawable.forest).fit().centerCrop().into(mBackgroundView);
+        mBackgroundView = (ImageView) findViewById(R.id.background);
+        mBlurredBackgroundView = (KenBurnsView) findViewById(R.id.blurred_background);
+
+        asyncLoadImage(R.drawable.forest);
+    }
+
+    private void asyncLoadImage(final int resId) {
+        Picasso.with(this).load(resId).fit().centerCrop().into(mBackgroundView);
+        Picasso.with(this).load(resId).fit().centerCrop().into(mBlurredBackgroundView, new Callback() {
+            @Override
+            public void onSuccess() {
+                asyncBlurBitmap(((BitmapDrawable) mBlurredBackgroundView.getDrawable()).getBitmap());
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
+    }
+
+    private void asyncBlurBitmap(final Bitmap bitmap) {
+        new ParallelAsyncTask<Void, Void, Bitmap>() {
+
+            @Override
+            protected Bitmap doInBackground(Void... params) {
+//                return BitmapFactory.blur(bitmap, 30);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                super.onPostExecute(bitmap);
+
+                mBlurredBackgroundView.setImageBitmap(bitmap);
+            }
+        }.execute();
     }
 }
